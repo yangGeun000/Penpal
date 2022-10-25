3c66fad85de69167e1eae8457cb1a9d98f1d9c26
juneon <tly013@naver.com>
2022-10-25 14:50:03 +0900

    refactor: 구조 수정

--- src/main/resources/templates/member/list/MemberFavoriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.penpal.project.member.list;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberFavoriteRepository extends JpaRepository<MemberFavorite, Integer>{
-
-}
--- src/main/resources/templates/member/list/MemberSnsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.penpal.project.member.list;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberSnsRepository extends JpaRepository<MemberSns, Integer>{
-
-}
