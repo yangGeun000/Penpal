a2f034c64f3933d1e8fe1220c07a65234332d823
USER <ruse217@gmail.com>
2022-10-26 22:43:10 +0900

    임시저장2

--- src/main/java/com/penpal/project/board/BoardService.java
+++ src/main/java/com/penpal/project/board/BoardService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.penpal.project.list.CategoryListRepository;
@@ -26,7 +27,26 @@ public class BoardService {
 	private final LocationListRepository locationListRepository;
 	private final CountryListRepository countryListRepository;
 
-	// by 장유란, 검색기능
+	// by 장유란, 검색기능(Specification 예시, 참고용)
+	//			BoardRepository의 findAllByKeyword의 쿼리와 유사기능
+//	private Specification<Board> search(String kw) {
+//        return new Specification<>() {
+//            private static final long serialVersionUID = 1L;
+//            @Override
+//            public Predicate toPredicate(Root<Board> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+//                query.distinct(true);// select distinct기능
+//                Join<Board, Member> u1 = q.join("writer", JoinType.LEFT);
+//                Join<Board, Answer> a = q.join("answerList", JoinType.LEFT);
+//                Join<Answer, Member> u2 = a.join("writer", JoinType.LEFT);
+//                return cb.or(cb.like(q.get("title"), "%" + kw + "%"), 	// 제목 
+//                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용 
+//                        cb.like(u1.get("memberId"), "%" + kw + "%"),    // 질문 작성자 
+//                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용 
+//                        cb.like(u2.get("memberId"), "%" + kw + "%"));   // 답변 작성자 
+//            }
+//		};
+//	}
+
 	public Page<Board> getList(int page, String kw) {
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
