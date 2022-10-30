cce30fd1de942f1d79400b975823663beeb89293
m5kt4nx <m5kt4nx@gmail.com>
2022-10-30 14:56:50 +0900

    style: 사이드 메뉴 템플릿 작성 및 관련 css & js 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,12 @@
-$('.header_navi').ready(function() {
+$('.header_navi').ready(function () {
 
 	var url = window.location.pathname,
 
-		urlRegExp = new RegExp(url.replace(/\/$/,) + "$");
+		urlRegExp = new RegExp(url.replace(/\/$/, ) + "$");
 
 	$('a').parent('li').removeClass('active');
-	$('a').each(function() {
-		if (urlRegExp.test(this.href.replace(/\/$/,))) {
+	$('a').each(function () {
+		if (urlRegExp.test(this.href.replace(/\/$/, ))) {
 			$(this).parent('li').addClass('active');
 		}
 	})
@@ -28,13 +28,13 @@ function CheckLoginInput() {
 } // 로그인 처리 기능
 //by 장유란, 페이징 js
 const page_elements = document.getElementsByClassName("page-link");
-Array.from(page_elements).forEach(function(element) {
-	element.addEventListener('click', function() {
+Array.from(page_elements).forEach(function (element) {
+	element.addEventListener('click', function () {
 		// by 장유란, 페이지 범위 밖으로 갈 수 없게 수정
-		if(this.dataset.page < 0 ) {
+		if (this.dataset.page < 0) {
 			this.dataset.page = 0
-		}else if(this.dataset.page >= page_elements.length-3){
-			this.dataset.page = page_elements.length-3;
+		} else if (this.dataset.page >= page_elements.length - 3) {
+			this.dataset.page = page_elements.length - 3;
 		}
 		document.getElementById('page').value = this.dataset.page;
 		document.getElementById('searchForm').submit();
@@ -43,10 +43,33 @@ Array.from(page_elements).forEach(function(element) {
 
 //by 장유란, 검색 js
 const btn_search = document.getElementById("btn_search");
-btn_search.addEventListener('click', function() {
-    document.getElementById('select_country').value = document.getElementById('country').value;
-    document.getElementById('select_location').value = document.getElementById('location').value;
-    document.getElementById('page').value = 0;  // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
-    document.getElementById('searchForm').submit();
+btn_search.addEventListener('click', function () {
+	document.getElementById('select_country').value = document.getElementById('country').value;
+	document.getElementById('select_location').value = document.getElementById('location').value;
+	document.getElementById('page').value = 0; // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
+	document.getElementById('searchForm').submit();
 });
 
+function sideMenuFunc() {
+	if ($("#menu_section").css("left") == '-320px') {
+
+		$("#menu_section").animate({
+			"left": "0px"
+		}, "Fast");
+	} else {
+		$("#menu_section").animate({
+			"left": "-320px"
+		}, "Fast");
+	}
+} // by 조성빈, 사이드 메뉴 열고 닫기 기능
+
+function openPopFriend() {
+	document.getElementById("pop_friend").style.display = "block";
+} 
+function openPopMessage() {
+	document.getElementById("pop_message").style.display = "block";
+}
+function closePop() {
+    document.getElementById("pop_friend").style.display = "none";
+    document.getElementById("pop_message").style.display = "none";
+} // by 조성빈, 사이드 메뉴에서 친구 목록 & 메시지 목록 열고 닫기 기능
