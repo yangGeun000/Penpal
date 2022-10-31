5b09c9151fbce890c4ff38e163cfdc0283bad248
m5kt4nx <m5kt4nx@gmail.com>
2022-10-31 11:52:03 +0900

    style: menu > 친구 관련 템플릿 작성 및 관련 css & js 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,7 +10,7 @@ $('.header_navi').ready(function () {
 			$(this).parent('li').addClass('active');
 		}
 	})
-}) // 메뉴에 active 클래스 추가하는 기능
+}) // by 조성빈, 상단 NAVI에 active 클래스 추가하는 기능
 
 function CheckLoginInput() {
 	var loginForm = document.loginForm;
@@ -22,10 +22,31 @@ function CheckLoginInput() {
 		return false;
 	}
 
-
-
 	loginForm.submit();
-} // 로그인 처리 기능
+} // by 조성빈, 로그인 처리 기능
+
+$(".friend_list_function_btn").children().click(function () {
+	if ($('button').hasClass("active")) {
+		$('button').removeClass("active")
+	}
+
+	$(this).addClass(" active");
+})// by 조성빈, 목록 & 요청 버튼 active 클래스 추가하는 기능
+
+$(".friend_list_section").ready(function(){
+	$(".friend_list_function_btn").click(function(){
+		if($(".friend_list_btn").hasClass("active")){
+			$(".my_friends").addClass("active");
+			$(".request_friend").removeClass("active");
+		}
+		
+		if($(".friend_request_btn").hasClass("active")){
+			$(".my_friends").removeClass("active");
+			$(".request_friend").addClass("active");
+		}
+	})
+})//by 조성빈, 목록 & 요청 버튼 클릭 시 list 영역 전환하는 기능
+
 //by 장유란, 페이징 js
 const page_elements = document.getElementsByClassName("page-link");
 Array.from(page_elements).forEach(function (element) {
@@ -65,11 +86,13 @@ function sideMenuFunc() {
 
 function openPopFriend() {
 	document.getElementById("pop_friend").style.display = "block";
-} 
+}
+
 function openPopMessage() {
 	document.getElementById("pop_message").style.display = "block";
 }
+
 function closePop() {
-    document.getElementById("pop_friend").style.display = "none";
-    document.getElementById("pop_message").style.display = "none";
+	document.getElementById("pop_friend").style.display = "none";
+	document.getElementById("pop_message").style.display = "none";
 } // by 조성빈, 사이드 메뉴에서 친구 목록 & 메시지 목록 열고 닫기 기능
