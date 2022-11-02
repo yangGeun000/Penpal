a438ed73e8584005b378018749a0f01d105a4658
YG <keylink000@gmail.com>
2022-11-02 12:10:43 +0900

    style: 레이아웃 최적화

--- src/main/java/com/penpal/project/MainController.java
+++ src/main/java/com/penpal/project/MainController.java
@@ -1,18 +1,7 @@
 package com.penpal.project;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.penpal.project.list.CountryList;
-import com.penpal.project.list.CountryListRepository;
-import com.penpal.project.list.LanguageList;
-import com.penpal.project.list.LanguageListRepository;
-import com.penpal.project.list.LocationList;
-import com.penpal.project.list.LocationListRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,48 +9,19 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class MainController {
 
-    private final LanguageListRepository languageListRepository;
-    private final LocationListRepository locationListRepository;
-    private final CountryListRepository countryListRepository;
-    
     @RequestMapping("/")
-    public String index(Model model){
-        model.addAttribute("index", "index");
+    public String index(){
         return "index";
     }
     
     @RequestMapping("/users")
-    public String users(Model model){
-        model.addAttribute("users", "users");
+    public String users(){
         return "member/users";
     }
 
     @RequestMapping("/users/profile")
-    public String userProfile(Model model){
-        model.addAttribute("profile", "profile");
+    public String userProfile(){
         return "member/user_profile";
     }
-
-
-
-
-    // by 조성빈, 상단 NAVI language 기능 사용을 위해 추가
-	@ModelAttribute("language")
-	public List<LanguageList> languageList(){
-		List<LanguageList> languageLists = languageListRepository.findAll();
-		return languageLists;
-	}
-
-    @ModelAttribute("location")
-	public List<LocationList> locationList() {
-		List<LocationList> locationLists = locationListRepository.findAll();
-		return locationLists;
-	}
-
-	@ModelAttribute("country")
-	public List<CountryList> countryList() {
-		List<CountryList> countryLists = countryListRepository.findAll();
-		return countryLists;
-	}
 
 }
