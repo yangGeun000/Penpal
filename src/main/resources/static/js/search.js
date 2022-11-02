b5de7fe75f7d072d939d11c886aad3096daa55db
m5kt4nx <m5kt4nx@gmail.com>
2022-11-02 17:23:12 +0900

    style: community 내 선택된 category에 active 넣는 기능 추가 및 관련 css & js 추가

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,7 +1,7 @@
 //by 장유란, 페이징 js
 const page_elements = document.getElementsByClassName("page-link");
-Array.from(page_elements).forEach(function(element) {
-	element.addEventListener('click', function() {
+Array.from(page_elements).forEach(function (element) {
+	element.addEventListener('click', function () {
 		// by 장유란, 페이지 범위 밖으로 갈 수 없게 수정
 		if (this.dataset.page < 0) {
 			this.dataset.page = 0
@@ -15,29 +15,37 @@ Array.from(page_elements).forEach(function(element) {
 
 
 var category = '';
-$(document).ready(function() {
-	$(".click-category").on('click', function(e) {
+$(document).ready(function () {
+	$(".click-category").on('click', function (e) {
 		category = (e.target.id);
 	});
 });
 
 const btn_list = document.getElementById("btn_list");
-btn_list.addEventListener('click', function() {
+btn_list.addEventListener('click', function () {
 	document.getElementById('select_category').value = category;
 	document.getElementById('select_location').value = "";
 	document.getElementById('select_country').value = "";
-	document.getElementById('page').value = 0;  // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
+	document.getElementById('page').value = 0; // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
 	document.getElementById('searchForm').submit();
 
 });
 
 //by 장유란, 검색 js
 const btn_search = document.getElementById("btn_search");
-btn_search.addEventListener('click', function() {
+btn_search.addEventListener('click', function () {
 	document.getElementById('kw').value = document.getElementById('search_kw').value;
 	document.getElementById('select_location').value = document.getElementById('location').value;
 	document.getElementById('select_country').value = document.getElementById('country').value;
-	document.getElementById('page').value = 0;  // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
+	document.getElementById('page').value = 0; // 검색버튼을 클릭할 경우 0페이지부터 조회한다.
 	document.getElementById('searchForm').submit();
 });
 
+window.onload = function () {
+	var selectCategory = document.getElementById('select_category').value;
+	var clickCategory = document.getElementsByClassName(selectCategory);
+	if (selectCategory != "") {
+		$("li").removeClass("active");
+		$(clickCategory).addClass("active");
+	}
+} // by 조성빈, 페이지 로드 후 선택된 category class에 active 넣는 기능
