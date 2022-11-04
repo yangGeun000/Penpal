7c1f8ebc24d0f617f627220caaac6ad17953f5f0
m5kt4nx <m5kt4nx@gmail.com>
2022-11-04 16:31:07 +0900

    style: 채팅방 닫기 버튼 적용 및 관련 js 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,12 @@
-$('.header_navi').ready(function() {
+$('.header_navi').ready(function () {
 
 	var url = window.location.pathname,
 
-		urlRegExp = new RegExp(url.replace(/\/$/,) + "$");
+		urlRegExp = new RegExp(url.replace(/\/$/, ) + "$");
 
 	$('a').parent('li').removeClass('active');
-	$('a').each(function() {
-		if (urlRegExp.test(this.href.replace(/\/$/,))) {
+	$('a').each(function () {
+		if (urlRegExp.test(this.href.replace(/\/$/, ))) {
 			$(this).parent('li').addClass('active');
 		}
 	})
@@ -20,49 +20,39 @@ $(".friend_list_function_btn").children().click(function () {
 	}
 
 	$(this).addClass(" active");
-})// by 조성빈, 목록 & 요청 버튼 active 클래스 추가하는 기능
+}) // by 조성빈, 목록 & 요청 버튼 active 클래스 추가하는 기능
 
-$(".friend_list_section").ready(function(){
-	$(".friend_list_function_btn").click(function(){
-		if($(".friend_list_btn").hasClass("active")){
+$(".friend_list_section").ready(function () {
+	$(".friend_list_function_btn").click(function () {
+		if ($(".friend_list_btn").hasClass("active")) {
 			$(".my_friends").addClass("active");
 			$(".request_friend").removeClass("active");
 		}
-		
-		if($(".friend_request_btn").hasClass("active")){
+
+		if ($(".friend_request_btn").hasClass("active")) {
 			$(".my_friends").removeClass("active");
 			$(".request_friend").addClass("active");
 		}
 	})
-})//by 조성빈, 친구 목록 & 친구 요청 버튼 클릭 시 list 영역 전환하는 기능
+}) //by 조성빈, 친구 목록 & 친구 요청 버튼 클릭 시 list 영역 전환하는 기능
 
 
-$(document).mouseup(function (e){
-	if($("#message_room").has(e.target).length === 0){
-		$("#message_room").hide();
-		$(".message_list_section").removeClass("active");
-	}
-
-	if($("#pop_friend").has(e.target).length === 0){
+$(document).mouseup(function (e) {
+	if ($("#pop_friend").has(e.target).length === 0) {
 		$("#pop_friend").hide();
 	}
 
-	if($("#pop_message").has(e.target).length === 0){
-		if($("#message_room").css("display") == "block"){
+	if ($("#pop_message").has(e.target).length === 0) {
+		if ($("#message_room").css("display") == "block") {
 			return false;
 		}
-	
-		if($("#message_room").css("display") == "none"){
+
+		if ($("#message_room").css("display") == "none") {
 			$(".message_list_seciton").removeClass("open_list")
 			$("#pop_message").hide();
-			document.removeEventListener("keypress", Enter);
-            if(ws != null){
-            	ws.close();
-            	console.log("소켓 종료");
-            }
 		}
 	}
-	
+
 }); //by 조성빈, 외부 클릭 시 레이어 팝업 숨기는 기능
 
 // by 장유란, paging, search 관련 js => search.js로 이동 
@@ -84,10 +74,21 @@ function openPopFriend() {
 	document.getElementById("pop_friend").style.display = "block";
 }
 
-function openMessageRoom(){
+function openMessageRoom() {
 	document.querySelector("#message_room").style.display = "block";
 	$(".message_list_section").addClass("active");
 }
+
+function closeMessageRoom() {
+	$("#message_room").hide();
+	$(".message_list_section").removeClass("active");
+
+	document.removeEventListener("keypress", Enter);
+	if (ws != null) {
+		ws.close();
+		console.log("소켓 종료");
+	}
+} // by 조성빈, 채팅받 닫기 기능
 
 function openPopMessage() {
 	document.getElementById("pop_message").style.display = "block";
