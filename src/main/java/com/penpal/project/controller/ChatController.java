05289ae51b1df9b829e35488a88d71e85f7b165f
YG <keylink000@gmail.com>
2023-01-08 07:44:49 +0900

    chore: 주석 수정

--- src/main/java/com/penpal/project/controller/ChatController.java
+++ src/main/java/com/penpal/project/controller/ChatController.java
@@ -23,56 +23,55 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class ChatController {
-	
+
 	private final RoomService roomService;
 	private final MemberService memberService;
-	
+
 	// by 구양근, 방을 검색하고 없으면 만들어서 반환
 	@RequestMapping("/requestRoom")
 	@ResponseBody
-	public  Room requestRoom(@RequestParam HashMap<Object, Object> params){
+	public Room requestRoom(@RequestParam HashMap<Object, Object> params) {
 		Member maker = this.memberService.getMember(Integer.parseInt((String) params.get("makerId")));
 		Member guest = this.memberService.getMember(Integer.parseInt((String) params.get("guestId")));
 		Room room = this.roomService.requestRoom(maker, guest);
 		return room;
 	}
-	
-	// by 구양근, 대화방 메세지 리스트 
-		@RequestMapping("/deleteRoom")
-		@ResponseBody
-		public  void deleteRoom(@RequestParam HashMap<Object, Object> params){
-			this.roomService.deleteRoom(Integer.parseInt((String) params.get("roomId")));
-		}
-	
+
+	// by 구양근, 대화방 삭제
+	@RequestMapping("/deleteRoom")
+	@ResponseBody
+	public void deleteRoom(@RequestParam HashMap<Object, Object> params) {
+		this.roomService.deleteRoom(Integer.parseInt((String) params.get("roomId")));
+	}
+
 	// by 구양근, 개인 대화방 리스트 가져오기
 	@RequestMapping("/getRoom")
 	@ResponseBody
-	public  List<Room> getRoom(@RequestParam HashMap<Object, Object> params){
+	public List<Room> getRoom(@RequestParam HashMap<Object, Object> params) {
 		Member member = this.memberService.getMember(Integer.parseInt((String) params.get("memberId")));
 		List<Room> roomList = new ArrayList<>();
 		roomList.addAll(member.getMakerList());
 		roomList.addAll(member.getGuestList());
 		// by 구양근, 대화방의 최근시간 순으로 정렬
-		roomList.sort((room1,room2) -> room2.getLastDate().compareTo(room1.getLastDate()));
-		
+		roomList.sort((room1, room2) -> room2.getLastDate().compareTo(room1.getLastDate()));
+
 		return roomList;
 	}
-	
-	// by 구양근, 대화방 메세지 리스트 
+
+	// by 구양근, 대화방 메세지 리스트
 	@RequestMapping("/getMessage")
 	@ResponseBody
-	public  List<Message> getMessage(@RequestParam HashMap<Object, Object> params, Principal principal){
+	public List<Message> getMessage(@RequestParam HashMap<Object, Object> params, Principal principal) {
 		Room room = this.roomService.getRoom(Integer.parseInt((String) params.get("roomId")));
 		List<Message> messageList = room.getMessageList();
-		if(room.getMaker().getMemberId().equals(principal.getName())){
+		if (room.getMaker().getMemberId().equals(principal.getName())) {
 			room.setMakerCount(0);
 			this.roomService.setRoom(room);
-		}
-		else {
+		} else {
 			room.setGuestCount(0);
 			this.roomService.setRoom(room);
 		}
 		return messageList;
 	}
-	
+
 }
